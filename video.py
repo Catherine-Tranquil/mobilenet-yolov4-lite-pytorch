2900482bedf5089143a98de3fdd89a4049dcd0fc
Camila <59348721+Catherine-Tranquil@users.noreply.github.com>
2021-05-16 21:46:10 +0800

    Update video.py

--- video.py
+++ video.py
@@ -15,14 +15,16 @@ from yolo import YOLO
 yolo = YOLO()
 #-------------------------------------#
 #   调用摄像头
-#   capture=cv2.VideoCapture("1.mp4")
+capture=cv2.VideoCapture("1.mp4")
 #-------------------------------------#
-capture=cv2.VideoCapture(0)
+#capture=cv2.VideoCapture(0)
 fps = 0.0
-while(True):
+while(capture.isOpened()):
     t1 = time.time()
     # 读取某一帧
-    ref,frame=capture.read()
+    ret,frame=capture.read()
+    if not ret:
+        break
     # 格式转变，BGRtoRGB
     frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
     # 转变成Image
@@ -36,9 +38,16 @@ while(True):
     print("fps= %.2f"%(fps))
     frame = cv2.putText(frame, "fps= %.2f"%(fps), (0, 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    cv2.imshow("video",frame)
+    # cv2.imshow("video",frame)
 
-    c= cv2.waitKey(1) & 0xff 
-    if c==27:
-        capture.release()
+    fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+    fps = int(capture.get(cv2.CAP_PROP_FPS))
+    video = cv2.VideoWriter('result.mp4', fourcc, fps, (1920, 1080))
+    video.write(frame)
+
+    if cv2.waitKey(fps) == 27:
         break
+    # c= cv2.waitKey(1) & 0xff
+    #if c==27:
+capture.release()
+video.release()
